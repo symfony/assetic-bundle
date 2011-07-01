bf7ee477c1192cb8f7e27697be0b5cd263b9015d
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-06-30 17:14:03 -0700

    removed unused use statement

--- DependencyInjection/AsseticExtension.php
+++ DependencyInjection/AsseticExtension.php
@@ -14,7 +14,6 @@ namespace Symfony\Bundle\AsseticBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
