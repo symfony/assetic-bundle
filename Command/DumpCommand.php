e54db79aedc6c8b68bccbf1d3d9a3964938e726a
Konstantin Kuklin <konstantin.kuklin@gmail.com>
2014-05-01 19:53:17 +0400

    Replaced point saving assets for big flexibility

--- Command/DumpCommand.php
+++ Command/DumpCommand.php
@@ -73,6 +73,19 @@ class DumpCommand extends ContainerAwareCommand
     }
 
     /**
+     * Wraper for saving assets
+     *
+     * @param String $filePath      The path to file
+     * @param String $information   The information for saving to file
+     */
+    protected function saveInformationToFile($filePath, $information)
+    {
+        if (false === @file_put_contents($filePath, $information)) {
+            throw new \RuntimeException('Unable to write file '.$filePath);
+        }
+    }
+
+    /**
      * Watches a asset manager for changes.
      *
      * This method includes an infinite loop the continuously polls the asset
@@ -203,8 +216,6 @@ class DumpCommand extends ContainerAwareCommand
             }
         }
 
-        if (false === @file_put_contents($target, $asset->dump())) {
-            throw new \RuntimeException('Unable to write file '.$target);
-        }
+        $this->saveInformationToFile($target, $asset->dump());
     }
 }
